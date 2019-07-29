d62fe0f645d89a7e408d046941774996fc04b8d3
olafl <olaflogin@protonmail.com>
2019-07-29 11:13:26 +0200

    y

--- app/src/main/java/com/oleaf/eighthours/calendar/Calendar.java
+++ app/src/main/java/com/oleaf/eighthours/calendar/Calendar.java
@@ -2,6 +2,7 @@ package com.oleaf.eighthours.calendar;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
 import com.oleaf.eighthours.R;
 
 public class Calendar extends AppCompatActivity {
@@ -9,6 +10,10 @@ public class Calendar extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        DisplayMetrics displayMetrics = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(displayMetrics);
+        int width = (int) (0.9f * displayMetrics.widthPixels);
+        getWindow().setLayout(width, width);
         setContentView(R.layout.activity_calendar);
     }
 }
